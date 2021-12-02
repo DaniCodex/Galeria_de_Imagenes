e88143c9d53d513cf78e3d8457516bc31db449bd
revolutiondaniel19@gmail.com <revolutiondaniel19@gmail.com>
2021-12-01 20:50:17 -0500

    cambio

---
+++ script.js
@@ -0,0 +1,10 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
